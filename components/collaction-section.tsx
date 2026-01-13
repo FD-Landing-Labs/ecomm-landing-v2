5204b59b05fd7070182305b65578ebce1be4f7ba
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-13 17:25:43 +0530

    feat: added image

--- components/collaction-section.tsx
+++ components/collaction-section.tsx
@@ -3,18 +3,27 @@ import Image from "next/image";
 export const CollactionSection = () => {
   return (
     <div className="flex flex-col gap-4 px-4  ">
-      <div className="bg-[#f6f6f6] rounded-lg p-4 text-black  flex justify-center">
-        Collaction
+      <div className="bg-[#f6f6f6] rounded-lg p-4 text-black/70  font-medium flex justify-center">
+        Collactions
       </div>
       <div className="flex justify-between gap-4">
-        <div>
+        <div className="relative">
           <Image
-            src="/assets/images/chair 2.webp"
+            src="/assets/images/c-chair2.avif"
             alt=""
-            width={700}
+            width={900}
             height={500}
-            className="rounded-lg object-cover w-full h-[515px]"
+            className="rounded-lg object-cover w-[950px] h-[515px]"
           />
+          <div className="absolute bottom-6 left-10 bg-white p-6 rounded-lg max-w-[250px] shadow-lg">
+            <h2 className="text-3xl font-medium text-black">Wood</h2>
+            <p className="mt-4 text-gray-500 text-sm leading-tight">
+              Our Wood Collection celebrates the natural beauty of wood.
+            </p>
+            <button className="mt-8 text-sm font-medium text-black underline hover:text-gray-700 transition-colors">
+              View Collection
+            </button>
+          </div>
         </div>
 
         <div className="flex flex-col gap-4">
@@ -22,7 +31,7 @@ export const CollactionSection = () => {
             <Image
               src="/assets/images/c-chair.avif"
               alt=""
-              width={400}
+              width={600}
               height={500}
               className="rounded-lg object-cover w-[350px]  h-[250px] "
             />
@@ -43,7 +52,8 @@ export const CollactionSection = () => {
             <div className="bg-black p-6 rounded-lg ">
               <h2 className="text-3xl font-medium text-white ">Modern</h2>
               <p className="mt-12 text-gray-500 text-sm leading-tight ">
-                The Modern Collection brings together graceful lines and luxurious finishes.
+                The Modern Collection brings together graceful lines and
+                luxurious finishes.
               </p>
 
               <button className="mt-8 text-sm font-medium text-white underline hover:text-gray-700 transition-colors">
