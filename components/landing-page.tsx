8005db138e064573c8bf38493cb27b061b3c85f6
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-13 18:16:28 +0530

    feat: imported about us page in landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "./hero-section";
 import { CollactionSection } from "./collaction-section";
+import { AboutUs } from "./about-us";
 
 export const LandingPage = () => {
   return (
     <div>
       <HeroSection />
       <CollactionSection />
+      <AboutUs />
     </div>
   );
 };
