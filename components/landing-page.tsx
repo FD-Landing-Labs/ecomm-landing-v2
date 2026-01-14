0620d9d604cf3018447679774c17d8d069474a61
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-14 10:58:28 +0530

    feat: imported favorite section in landinge page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -2,14 +2,17 @@ import { HeroSection } from "./hero-section";
 import { CollactionSection } from "./collaction-section";
 import { AboutUs } from "./about-us";
 import  Footer  from "./footer";
+import { OurFavoritesSection } from "./our-favorites-section";
 
 export const LandingPage = () => {
   return (
     <div>
       <HeroSection />
+      <OurFavoritesSection />
       <CollactionSection />
       <AboutUs />
       <Footer />
     </div>
   );
 };
+``
